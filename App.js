983fa324c15c544880e0d12bed9dd5597662be9f
iiggaarr <iiggaarr19912004@gmail.com>
2020-02-07 12:13:54 +0300

    init app

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: "white"}}>Аf</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

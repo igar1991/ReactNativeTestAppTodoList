f4a0e520a18a179dc8e6deeb629758b025bc2d1e
iiggaarr <iiggaarr19912004@gmail.com>
2020-02-10 20:21:45 +0300

    AddModal 1

---
+++ src/components/AddModal.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import { StyleSheet, TextInput, Button, View, Alert, Modal } from 'react-native';
+
+export const AddModal = ({}) => {
+
+    return (
+        <Modal>
+            <View>
+                
+            </View>
+        </Modal>
+
+    )
+}
+
+const styles = StyleSheet.create({})
